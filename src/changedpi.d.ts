899de5fe39a6c26b86ca30467a356e10738fad42
OREO1210 <bittoo979@gmail.com>
2022-10-10 18:42:35 +0530

    .

--- src/changedpi.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'changedpi' {
-  export function changeDpiBlob(blob: Blob, dpi: number): Promise<Blob>
-  export function changeDpiDataUrl(base64Image: string, dpi: number): string
-}
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { ViteSSG } from 'vite-ssg'
-import { routes } from './router'
-import App from './App.vue'
-
-import './styles/tailwind.css'
-
-export const createApp = ViteSSG(App, { routes })
